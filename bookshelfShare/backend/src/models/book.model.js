0d394c93933aa2e09d961fde3a7d00de8fce0b9e
Xavier Lasierra <xlp1998@gmail.com>
2021-08-30 09:34:33 +0200

    Create Book model

---
+++ bookshelfShare/backend/src/models/book.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  title: String,
+  subtitle: String,
+  author: [String],
+  publisher: String,
+  publishedDate: Date,
+  description: String,
+  ISBN: {
+    ISBN10: String,
+    ISBN13: String
+  },
+  pageCount: Number,
+  format: String,
+  categories: [String],
+  images: {
+    smallThumbnail: String,
+    thumbnail: String
+  },
+  rating: [
+    {
+      rate: Number,
+      review: String,
+      user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+    }
+  ]
+});
+
+module.exports = mongoose.model('User', userSchema);
