ae0be67484c01e72c1734e288bdbc5e7435bf8ee
Xavier Lasierra <xlp1998@gmail.com>
2021-08-30 13:11:01 +0200

    Add error handle to registerUser controller

--- bookshelfShare/backend/src/controllers/auth.controller.js
+++ bookshelfShare/backend/src/controllers/auth.controller.js
@@ -1,12 +1,17 @@
 const User = require('../models/user.model');
 
 async function registerUser({ user, body: { username } }, res) {
-  const createdUser = await User.create({ ...user, username });
+  try {
+    const createdUser = await User.create({ ...user, username });
 
-  res.json({
-    createdUser,
-    message: 'User registered'
-  });
+    res.json({
+      createdUser,
+      message: 'User registered'
+    });
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
 }
 
 module.exports = {
