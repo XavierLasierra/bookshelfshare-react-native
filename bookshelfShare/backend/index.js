510a154340da237e6a1dde9af0d0a69e79cfc812
Xavier Lasierra <xlp1998@gmail.com>
2021-08-30 10:52:06 +0200

    Create and define basic auth controller structure
    -Create register, login, refresh token and logout routes

--- bookshelfShare/backend/index.js
+++ bookshelfShare/backend/index.js
@@ -4,17 +4,20 @@ const debug = require('debug')('booksSApi');
 const chalk = require('chalk');
 const morgan = require('morgan');
 const cors = require('cors');
+const authRouter = require('./src/routes/auth.router');
 
 require('./src/config/ddbb.config');
 
 const server = express();
 const port = process.env.PORT || 5000;
 
+require('./src/config/passport.config')(server);
+
 server.use(cors());
 server.use(morgan('dev'));
 server.use(express.json());
 
-// TODO routers
+server.use('/api/auth', authRouter);
 
 server.listen(
   port,
