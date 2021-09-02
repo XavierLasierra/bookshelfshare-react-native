5565a70758c5ca0231fbdf014ca011ed4c10255f
Xavier Lasierra <xlp1998@gmail.com>
2021-09-02 16:01:33 +0200

    Configure aplication to use RNCamera

--- bookshelfShare/frontend/App.tsx
+++ bookshelfShare/frontend/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
+import { MenuProvider } from 'react-native-popup-menu';
 
 import MainNavigator from './src/navigators/MainNavigator/MainNavigator';
 
@@ -9,9 +10,11 @@ import configureStore from './src/redux/store';
 export default function App() {
   return (
     <Provider store={configureStore()}>
-      <NavigationContainer>
-        <MainNavigator />
-      </NavigationContainer>
+      <MenuProvider>
+        <NavigationContainer>
+          <MainNavigator />
+        </NavigationContainer>
+      </MenuProvider>
     </Provider>
   );
 }
