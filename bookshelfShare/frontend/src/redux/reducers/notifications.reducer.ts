693491f0572efaecacc6e37fcededb531105dfe5
Xavier Lasierra <xlp1998@gmail.com>
2021-09-03 12:57:16 +0200

    Create a reducer for snack bar notifications

---
+++ bookshelfShare/frontend/src/redux/reducers/notifications.reducer.ts
@@ -0,0 +1,29 @@
+import notificationsActions from '../actions/notifications.actions';
+
+interface Action {
+    type: string
+}
+
+function notificationsReducer(notification = [], action: Action): string[] {
+  let newNotification: string[] = notification;
+  switch (action.type) {
+    case notificationsActions.LOGIN_ERROR:
+      newNotification = ['Invalid email or password'];
+      break;
+    case notificationsActions.SERVER_ERROR:
+      newNotification = ['Server error'];
+      break;
+    case notificationsActions.REGISTER_USER:
+      newNotification = ['User correctly registered'];
+      break;
+    case notificationsActions.REGISTER_ERROR:
+      newNotification = ['User already registered'];
+      break;
+    default:
+      break;
+  }
+
+  return newNotification;
+}
+
+export default notificationsReducer;
