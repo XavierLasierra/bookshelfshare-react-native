4634758d3116105fa73c704a01f8a913dc9faba4
Xavier Lasierra <xlp1998@gmail.com>
2021-09-03 13:00:45 +0200

    Add functionality to Register component
    -Create an action creator for register.
    -Add validations to each TextInput.

--- bookshelfShare/frontend/src/redux/reducers/index.ts
+++ bookshelfShare/frontend/src/redux/reducers/index.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import loggedUser from './loggedUser.reducer';
+import notifications from './notifications.reducer';
 
 export default combineReducers({
-  loggedUser
+  loggedUser,
+  notifications
 });
