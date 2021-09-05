e7e1443b09b7a39630a3ebb515e717d03ab1fdb3
Xavier Lasierra <xlp1998@gmail.com>
2021-09-06 01:50:41 +0200

    Clear isbn navigation parameter when dismounting BookSearchNavigator

--- bookshelfShare/frontend/src/navigators/BookSearchNavigator/BookSearchNavigator.tsx
+++ bookshelfShare/frontend/src/navigators/BookSearchNavigator/BookSearchNavigator.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 
 import BookSearch from '../../components/BookSearch/BookSearch';
@@ -6,6 +6,7 @@ import BookResults from '../../components/BookResults/BookResults';
 
 interface Props {
     route: Route
+    navigation: any
 }
 interface Route {
   params: Params
@@ -14,8 +15,13 @@ interface Params {
   isbn?: string,
 }
 
-export default function BookSearchNavigator({ route: { params } }: Props) {
+export default function BookSearchNavigator(
+  { route: { params }, navigation: pagesNavigation }: Props
+) {
   const Stack = createStackNavigator();
+  useEffect(() => () => {
+    pagesNavigation.setParams({ isbn: '' });
+  }, []);
   return (
     <Stack.Navigator
       initialRouteName="BookSearch"
