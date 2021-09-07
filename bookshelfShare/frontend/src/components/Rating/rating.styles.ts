8a3b46b0bc1cf55340daa2a76da458046729de8e
Xavier Lasierra <xlp1998@gmail.com>
2021-09-07 10:59:09 +0200

    Add filter functionality to book reviews

--- bookshelfShare/frontend/src/components/Rating/rating.styles.ts
+++ bookshelfShare/frontend/src/components/Rating/rating.styles.ts
@@ -2,23 +2,53 @@ import { StyleSheet } from 'react-native';
 import stylesConstants from '../../styles/styles.constants';
 
 export default StyleSheet.create({
-  text: {
-    fontSize: stylesConstants.fontSize.mid
-  },
-  titleText: {
-    fontSize: stylesConstants.fontSize.mid,
-    fontWeight: 'bold',
-    marginTop: 5
-  },
   ratingInput: {
     backgroundColor: stylesConstants.colors.white,
     height: 100,
-    textAlignVertical: 'top'
+    textAlignVertical: 'top',
+    fontSize: stylesConstants.fontSize.mid,
+    padding: stylesConstants.padding.small
   },
   topContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
     marginTop: 20,
     marginBottom: 10
+  },
+  usersReviewsContainer: {
+    marginTop: 20
+  },
+  ratingTitle: {
+    marginBottom: 10
+  },
+  ratingFilterContainer: {
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: stylesConstants.colors.dark,
+    marginBottom: 20
+  },
+  ratingFilterOption: {
+    width: 225,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 5,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: stylesConstants.colors.secondary
+  },
+  reviewNumberText: {
+    fontSize: stylesConstants.fontSize.mid
+  },
+  allReviewsText: {
+    width: '100%',
+    textAlign: 'center',
+    fontSize: stylesConstants.fontSize.mid,
+    paddingVertical: 5
+  },
+  activityIndicator: {
+    height: 30,
+    width: 30
   }
 });
