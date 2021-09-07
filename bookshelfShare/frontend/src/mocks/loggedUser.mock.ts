3612678bac8fe4e56c128ff7f1d026882c03a3d5
Xavier Lasierra <xlp1998@gmail.com>
2021-09-07 22:54:22 +0200

    Create loggedUser mock

---
+++ bookshelfShare/frontend/src/mocks/loggedUser.mock.ts
@@ -0,0 +1,11 @@
+export default {
+  user: {
+    _id: '1',
+    username: 'Xavier Lasierra',
+    email: 'xlp1998@gmail.com',
+    password: '123',
+    activity: []
+  },
+  token: 'token',
+  refreshToken: 'refreshToken'
+};
