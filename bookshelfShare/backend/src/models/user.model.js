31c013ba36b1a484aba7a87d39cb7db010f31d80
Xavier Lasierra <xlp1998@gmail.com>
2021-09-08 17:37:34 +0200

    Refactor user model

--- bookshelfShare/backend/src/models/user.model.js
+++ bookshelfShare/backend/src/models/user.model.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const defaultPhoto = require('../constants/defaultProfileImage');
 
 const userSchema = mongoose.Schema({
   username: String,
   email: String,
   password: String,
+  photo: { type: String, default: defaultPhoto },
   activity: [
     {
       bookId: { type: mongoose.Schema.Types.ObjectId, ref: 'Book' },
@@ -12,7 +14,13 @@ const userSchema = mongoose.Schema({
       rating: Number,
       description: String
     }
-  ]
+  ],
+  books: {
+    read: [String],
+    pending: [String],
+    whishlist: [String],
+    current: [String]
+  }
 });
 
 userSchema.methods.isValidPassword = function isValidPassword(password) {
