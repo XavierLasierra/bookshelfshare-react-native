236edd38fe873650a7319b468300d5c02c50d448
Xavier Lasierra <xlp1998@gmail.com>
2021-09-08 19:07:17 +0200

    Add book controller tests

--- bookshelfShare/backend/src/controllers/books.controller.test.js
+++ bookshelfShare/backend/src/controllers/books.controller.test.js
@@ -1,11 +1,16 @@
 const axios = require('axios');
 const {
   getBooks,
-  createGoogleSearchUrl
+  createGoogleSearchUrl,
+  getBookRating,
+  addUpdateBookRating
 } = require('./books.controller');
 const googleBooksMock = require('../mocks/googleBooks.mock');
+const Book = require('../models/book.model');
+const ratingsMock = require('../mocks/ratings.mock');
 
 jest.mock('axios');
+jest.mock('../models/book.model');
 
 describe('Given a getBooks function', () => {
   describe('When it is triggered', () => {
@@ -67,6 +72,159 @@ describe('Given a createGoogleSearchUrl', () => {
 
       test('Then the returned url should not contain 1984+', () => {
         expect(url).not.toContain('1984+');
+      });
+    });
+  });
+});
+
+describe('Given a getBookRating function', () => {
+  describe('When it is triggered', () => {
+    let req;
+    let res;
+    beforeEach(() => {
+      req = {
+        params: {
+          bookIsbn: '1234'
+        }
+      };
+      res = {
+        send: jest.fn(),
+        status: jest.fn(),
+        json: jest.fn()
+      };
+    });
+
+    describe('And findOne is resolved', () => {
+      describe('And foundBook is not undefined', () => {
+        test('Then should call res.json with the object the promise resolves with', async () => {
+          const foundBook = { foundBook: 'foundBook' };
+          Book.findOne.mockReturnValue({
+            populate: jest.fn().mockResolvedValue(foundBook)
+          });
+
+          await getBookRating(req, res);
+
+          expect(res.json).toHaveBeenCalledWith(foundBook);
+        });
+      });
+
+      describe('And foundBook is undefined', () => {
+        test('Then should call res.json with an object with an empty array in ratings property', async () => {
+          Book.findOne.mockReturnValue({
+            populate: jest.fn().mockResolvedValue(undefined)
+          });
+
+          await getBookRating(req, res);
+
+          expect(res.json).toHaveBeenCalledWith({ ratings: [] });
+        });
+      });
+    });
+
+    describe('And findOne is rejected', () => {
+      beforeEach(async () => {
+        Book.findOne.mockReturnValue({
+          populate: jest.fn().mockRejectedValue(new Error('Server error'))
+        });
+        await getBookRating(req, res);
+      });
+
+      test('Then should call res.status with 500', () => {
+        expect(res.status).toHaveBeenCalledWith(500);
+      });
+
+      test('Then should call res.send with an error with a message Server error', () => {
+        expect(res.send).toHaveBeenCalled();
+      });
+    });
+  });
+});
+
+describe('Given a addUpdateBookRating function', () => {
+  describe('When it is triggered', () => {
+    let req;
+    let res;
+    beforeEach(() => {
+      req = {
+        params: {
+          bookIsbn: '1234'
+        },
+        body: {
+          user: '',
+          rating: '',
+          review: ''
+        }
+      };
+      res = {
+        send: jest.fn(),
+        status: jest.fn(),
+        json: jest.fn()
+      };
+    });
+
+    describe('And findOne is resolved', () => {
+      describe('And foundBook is not undefined', () => {
+        beforeEach(() => {
+          Book.findOne.mockResolvedValue({
+            ...ratingsMock,
+            save: jest.fn().mockResolvedValue({
+              populate: jest.fn().mockResolvedValue({})
+            })
+          });
+        });
+        describe('And there is a rating.user equal than body.user', () => {
+          beforeEach(() => {
+            req.body.user = '1';
+          });
+
+          test('Then res.json should have been called', async () => {
+            await addUpdateBookRating(req, res);
+
+            expect(res.json).toHaveBeenCalled();
+          });
+        });
+        describe('And there is not rating.user equal than body.user', () => {
+          test('Then res.json should have been called', async () => {
+            await addUpdateBookRating(req, res);
+
+            expect(res.json).toHaveBeenCalled();
+          });
+        });
+      });
+
+      describe('And foundBook is undefined', () => {
+        beforeEach(async () => {
+          Book.findOne.mockResolvedValue(undefined);
+          Book.create.mockResolvedValue({
+            ...ratingsMock,
+            save: jest.fn().mockResolvedValue({
+              populate: jest.fn().mockResolvedValue({})
+            })
+          });
+          await addUpdateBookRating(req, res);
+        });
+        test('Then Book.create should have been called', () => {
+          expect(Book.create).toHaveBeenCalled();
+        });
+
+        test('Then res.json should have been called', () => {
+          expect(res.json).toHaveBeenCalled();
+        });
+      });
+    });
+
+    describe('And findOne is rejected', () => {
+      beforeEach(async () => {
+        Book.findOne.mockRejectedValue(new Error('Server error'));
+        await addUpdateBookRating(req, res);
+      });
+
+      test('Then should call res.status with 500', () => {
+        expect(res.status).toHaveBeenCalledWith(500);
+      });
+
+      test('Then should call res.send with an error with a message Server error', () => {
+        expect(res.send).toHaveBeenCalled();
       });
     });
   });
