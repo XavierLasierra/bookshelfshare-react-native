0fb1c04cf5f381965c1b9a9d9f75a96cb10e88dd
Xavier Lasierra <xlp1998@gmail.com>
2021-09-08 22:51:48 +0200

    Create Profile component basic structure

--- bookshelfShare/frontend/src/components/Profile/Profile.tsx
+++ bookshelfShare/frontend/src/components/Profile/Profile.tsx
@@ -1,13 +1,91 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import {
+  SafeAreaView, View, Image, Text, TouchableOpacity
+} from 'react-native';
+import { useSelector } from 'react-redux';
 import Header from '../Header/Header';
 
 import ProfileIcon from '../../assets/profileIcon.svg';
+import BookStackIcon from '../../assets/bookStackIcon.svg';
+import styles from './profile.styles';
 
 export default function Profile() {
+  const { userData } = useSelector((store: any) => store.loggedUser);
+  const shelves = useSelector((store: any) => store.shelves);
   return (
     <SafeAreaView>
       <Header Logo={ProfileIcon} />
+      <View style={styles.profileContainer}>
+        <View style={styles.topContainer}>
+          <Image
+            source={{ uri: userData.photo }}
+            style={styles.userPhoto}
+          />
+          <View style={styles.userInformationContainer}>
+            <Text style={styles.userUsername}>{userData.username}</Text>
+            <Text style={styles.userEmail}>{userData.email}</Text>
+          </View>
+
+        </View>
+        <View>
+          <TouchableOpacity style={styles.profileButton}>
+            <BookStackIcon width={40} height={40} />
+            <Text>
+              {userData.books.current.length}
+              {' '}
+              {userData.books.current.length === 1 ? 'book' : 'books'}
+              {' '}
+              currently reading
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.profileButton}>
+            <BookStackIcon width={40} height={40} />
+            <Text>
+              {userData.books.read.length}
+              {' '}
+              {userData.books.read.length === 1 ? 'book' : 'books'}
+              {' '}
+              read
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.profileButton}>
+            <BookStackIcon width={40} height={40} />
+            <Text>
+              {userData.books.wishlist.length}
+              {' '}
+              {userData.books.wishlist.length === 1 ? 'book' : 'books'}
+              {' '}
+              on wishlist
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.profileButton}>
+            <BookStackIcon width={40} height={40} />
+            <Text>
+              {userData.followers.length}
+              {' '}
+              {userData.followers.length === 1 ? 'follower' : 'followers'}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.profileButton}>
+            <BookStackIcon width={40} height={40} />
+            <Text>
+              {userData.following.length}
+              {' '}
+              {userData.following.length === 1 ? 'user' : 'users'}
+              {' '}
+              following
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.profileButton}>
+            <BookStackIcon width={40} height={40} />
+            <Text>
+              {shelves.length}
+              {' '}
+              {shelves.length === 1 ? 'shelf' : 'shelves'}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
---
+++ bookshelfShare/frontend/src/components/Profile/profile.styles.ts
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+import stylesConstants from '../../styles/styles.constants';
+
+export default StyleSheet.create({
+  profileContainer: {
+    paddingHorizontal: stylesConstants.padding.normal
+  },
+  topContainer: {
+    paddingVertical: stylesConstants.padding.small,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  userPhoto: {
+    width: 125,
+    height: 125,
+    borderRadius: 100,
+    marginRight: 30
+  },
+  userInformationContainer: {
+    maxWidth: 200
+  },
+  userUsername: {
+    fontSize: stylesConstants.fontSize.big,
+    fontWeight: 'bold',
+    marginBottom: 15
+  },
+  userEmail: {
+    fontSize: stylesConstants.fontSize.mid
+  },
+  profileButton: {
+    flexDirection: 'row'
+  }
+});
