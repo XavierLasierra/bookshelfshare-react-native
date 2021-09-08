991d18714c14e3bf7f73b0833c4cba4ecec62798
Xavier Lasierra <xlp1998@gmail.com>
2021-09-08 22:49:27 +0200

    Create basic shelves reducer

--- bookshelfShare/frontend/src/redux/reducers/index.ts
+++ bookshelfShare/frontend/src/redux/reducers/index.ts
@@ -4,11 +4,13 @@ import tokens from './tokens.reducer';
 import notifications from './notifications.reducer';
 import books from './books.reducer';
 import customBookData from './customBookData.reducer';
+import shelves from './shelves.reducer';
 
 export default combineReducers({
   loggedUser,
   tokens,
   notifications,
   books,
-  customBookData
+  customBookData,
+  shelves
 });
