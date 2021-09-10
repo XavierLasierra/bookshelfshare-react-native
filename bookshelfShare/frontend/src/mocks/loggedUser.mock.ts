966d1ebd9f499284b8f427db3a1c7e5b7b37059a
Xavier Lasierra <xlp1998@gmail.com>
2021-09-10 15:34:12 +0200

    Add ProfileNavigator components tests

--- bookshelfShare/frontend/src/mocks/loggedUser.mock.ts
+++ bookshelfShare/frontend/src/mocks/loggedUser.mock.ts
@@ -3,7 +3,8 @@ export default {
     _id: '1',
     username: 'Xavier Lasierra',
     email: 'xlp1998@gmail.com',
-    activity: []
+    activity: [],
+    photo: 'photo'
   },
   token: 'token',
   refreshToken: 'refreshToken'
---
+++ bookshelfShare/frontend/src/navigators/ProfileNavigator/profileNavigator.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import ProfileNavigator from './ProfileNavigator';
+import { render } from '../../utils/test.utils';
+import loggedUserMock from '../../mocks/loggedUser.mock';
+import userBooksMock from '../../mocks/userBooks.mock';
+
+jest.useFakeTimers();
+
+describe('Given a ProfileNavigator component', () => {
+  describe('When it is rendered', () => {
+    test('Then should match the snapshot', () => {
+      const initialState = {
+        loggedUser: { userData: loggedUserMock.user },
+        userBooks: userBooksMock
+      };
+      const screen = render(
+        <NavigationContainer>
+          <ProfileNavigator />
+        </NavigationContainer>,
+        initialState
+      );
+      expect(screen).toMatchSnapshot();
+    });
+  });
+});
