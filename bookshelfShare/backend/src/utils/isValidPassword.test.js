e64d5cf778198cf6da47a02f6d1c72b402a35ab6
Xavier Lasierra <xlp1998@gmail.com>
2021-09-10 09:57:41 +0200

    Move isValidPassword function to a separate file and add test to it

---
+++ bookshelfShare/backend/src/utils/isValidPassword.test.js
@@ -0,0 +1,16 @@
+const bcrypt = require('bcrypt');
+const { isValidPassword } = require('./isValidPassword');
+
+jest.mock('bcrypt');
+
+describe('Given a isValidPassword function', () => {
+  describe('When it is triggered', () => {
+    test('Then should call bcrypt.compareSync', () => {
+      const password = '1234';
+      bcrypt.compareSync.mockImplementation();
+      isValidPassword(password);
+
+      expect(bcrypt.compareSync).toHaveBeenCalled();
+    });
+  });
+});
