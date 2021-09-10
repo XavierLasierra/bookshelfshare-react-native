9bb0f21c4bd26581bf970c1bdf8ee88e5fce252c
Xavier Lasierra <xlp1998@gmail.com>
2021-09-10 14:53:26 +0200

    Create FollowingNavigator tests

---
+++ bookshelfShare/frontend/src/navigators/FollowingNavigator/followingNavigator.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import FollowingNavigator from './FollowingNavigator';
+import { render } from '../../utils/test.utils';
+
+jest.useFakeTimers();
+
+describe('Given a FollowingNavigator component', () => {
+  describe('When it is rendered', () => {
+    test('Then should match the snapshot', () => {
+      const screen = render(
+        <NavigationContainer>
+          <FollowingNavigator />
+        </NavigationContainer>
+      );
+      expect(screen).toMatchSnapshot();
+    });
+  });
+});
