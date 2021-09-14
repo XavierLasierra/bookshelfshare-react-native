a28b59e1a7bba7fc60e52bc8f032a5c0e77bd443
Xavier Lasierra <xlp1998@gmail.com>
2021-09-14 16:43:40 +0200

    Create ShelfMenu component tests

---
+++ bookshelfShare/frontend/src/components/ShelfMenu/shelfMenu.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { MenuProvider } from 'react-native-popup-menu';
+import ShelfMenu from './ShelfMenu';
+import { render } from '../../utils/test.utils';
+import shelfMock from '../../mocks/shelf.mock';
+
+const navigation = {
+  push: jest.fn()
+};
+
+describe('Given a ShelfMenu component', () => {
+  describe('When it is rendered', () => {
+    describe('And shelves does not include bookIsbn', () => {
+      test('Then should match the snapshot', () => {
+        const initialState = { userShelves: [shelfMock] };
+        const screen = render(
+          <MenuProvider>
+            <ShelfMenu navigation={navigation} bookIsbn="0" />
+          </MenuProvider>,
+          initialState
+        );
+        expect(screen).toMatchSnapshot();
+      });
+    });
+    describe('And shelves does not include bookIsbn', () => {
+      test('Then should match the snapshot', () => {
+        const initialState = { userShelves: [shelfMock] };
+        const screen = render(
+          <MenuProvider>
+            <ShelfMenu navigation={navigation} bookIsbn="1" />
+          </MenuProvider>,
+          initialState
+        );
+        expect(screen).toMatchSnapshot();
+      });
+    });
+  });
+});
